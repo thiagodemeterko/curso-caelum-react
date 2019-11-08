1811cca6af6ed84f3ad314b0947d10b74b74efbc
thiagodemeterko <thiago.demeterko@gmail.com>
2019-11-07 22:44:54 -0300

    ate aula 16

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -5,6 +5,61 @@ import Widget from '../../components/Widget'
 import './loginPage.css'
 
 class LoginPage extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            erro: ""
+        };
+    }  
+
+    fazerLogin = infosDoEvento => {
+        infosDoEvento.preventDefault();
+
+        const dadosDeLogin = {
+            login: this.refs.inputLogin.value,
+            senha: this.refs.inputSenha.value
+        };
+
+        fetch("https://twitelum-api.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dadosDeLogin)
+        })
+        .then(async responseDoServer => {
+            if (!responseDoServer.ok) {
+                const respostaDeErroDoServidor = await responseDoServer.json();
+                const errorObj = Error(respostaDeErroDoServidor.message);
+                errorObj.status = responseDoServer.status;
+                throw errorObj;
+            }
+            return responseDoServer.json();
+        })
+        .then(dadosDoServidorEmObj => {
+            const token = dadosDoServidorEmObj.token;
+            if(token) {
+                localStorage.setItem("TOKEN", token);
+                this.props.history.push("/");
+            }
+            this.setState({
+                erro: ""
+            })
+        })
+        .catch(err => {
+            console.error(`[Erro ${err.status}]`, err.message);
+
+            this.setState({
+                erro: err.message
+            }, () => 
+                setTimeout(() => this.setState({
+                    erro: ""
+                }), 1000)
+            )
+        });
+    }
+
     render() {
         return (
             <Fragment>
@@ -13,18 +68,22 @@ class LoginPage extends Component {
                     <div className="container">
                         <Widget>
                             <h2 className="loginPage__title">Seja bem vindo!</h2>
-                            <form className="loginPage__form" action="/">
+                            <form className="loginPage__form" action="/" onSubmit={this.fazerLogin}>
                                 <div className="loginPage__inputWrap">
                                     <label className="loginPage__label" htmlFor="login">Login</label> 
-                                    <input className="loginPage__input" type="text" id="login" name="login"/>
+                                    <input ref="inputLogin" className="loginPage__input" type="text" id="login" name="login"/>
                                 </div>
                                 <div className="loginPage__inputWrap">
                                     <label className="loginPage__label" htmlFor="senha">Senha</label> 
-                                    <input className="loginPage__input" type="password" id="senha" name="senha"/>
+                                    <input ref="inputSenha" className="loginPage__input" type="password" id="senha" name="senha"/>
                                 </div>
-                                {/* <div className="loginPage__errorBox">
-                                    Mensagem de erro!
-                                </div> */}
+                                {
+                                    this.state.erro 
+                                    &&
+                                    <div className="loginPage__errorBox">
+                                        {this.state.erro}
+                                    </div>
+                                }
                                 <div className="loginPage__inputWrap">
                                     <button className="loginPage__btnLogin" type="submit">
                                         Logar
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,36 @@
-import React from "react";
-import {Switch, Route} from "react-router-dom";
+import React, {Component} from "react";
+import {Switch, Route, Redirect} from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage"
 
-class Roteamento extends React.Component {
+class PrivateRoute extends Component {
+    estaAutenticado = () => {
+        if(localStorage.getItem('TOKEN')) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    render() {
+        const { component: Component, ...props} = this.props
+        if(this.estaAutenticado()) {
+            return <Component {...props} />
+        } else {
+            return <Redirect to="/login" />
+        }
+    }
+}
+
+class Roteamento extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/" component={HomePage} exact />
+                <PrivateRoute path="/" component={HomePage} exact />
                 <Route path="/login" component={LoginPage} />
+                <Route component={NotFoundPage}/>
             </Switch>
         );
     }
